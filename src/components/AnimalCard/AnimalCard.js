e736aee84e352ea9197bdc235cfc4dffa851bdb0
topcook <codecooker1996@gmail.com>
2022-01-16 11:05:42 -0800

    I added propTypes and default props to AnimalCard component.

--- src/components/AnimalCard/AnimalCard.js
+++ src/components/AnimalCard/AnimalCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './AnimalCard.css';
+import PropTypes from 'prop-types';
 
 export default function AnimalCard(props) {
     return(
@@ -11,4 +12,22 @@ export default function AnimalCard(props) {
             <button onClick = {() => props.showAdditional(props.additional)}>More Info</button>
         </div>
     )
+}
+
+AnimalCard.propTypes = {
+    additional: PropTypes.shape({
+        link: PropTypes.string,
+        notes:PropTypes.string
+    }),
+    diet: PropTypes.arrayOf(PropTypes.string).isRequired,
+    name: PropTypes.string.isRequired,
+    scientificName: PropTypes.string.isRequired,
+    showAdditional: PropTypes.func.isRequired,
+    size: PropTypes.number.isRequired
+}
+
+AnimalCard.defaultProps = {
+    additional:{
+        notes: 'No Additional Information'
+    }
 }
