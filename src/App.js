7956ee217b9992f24c350cdae6d72c079b0caf20
topcook <codecooker1996@gmail.com>
2022-01-16 05:38:34 -0800

    I added clicking event to elements.

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import './App.css';
 function App() {
   const greeting = 'greeting';
 
+  const displayEmojiName = event => alert(event.target.id);
+
   return (
     <div className='container'>
       <h1 id={greeting} >Hello World!</h1>
       <p>I'm writing JSX</p>
       <ul>
         <li>
-          <button>
+          <button onClick = {displayEmojiName}>
             <span role="img" aria-label="grinning face" id="grinning face" >&#x1F600;</span>
           </button>
         </li>
         <li>
-          <button>
+          <button onClick = {displayEmojiName}>
             <span role="img" aria-label="party popper" id="party popper">&#x1F389;</span>
           </button>
         </li>
         <li>
-          <button>
+          <button onClick = {displayEmojiName}  >
             <span role="img" aria-label="woman dancing" id="woman dancing" >&#x1F483;</span>
           </button>
         </li>
