ab9e282bb7918d14c7ee525d158508e018f8828a
topcook <codecooker1996@gmail.com>
2022-01-16 05:32:24 -0800

    I added styles to the elements with JSX.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import './App.css';
 
 function App() {
-  return null;
+  const greeting = 'greeting';
+
+  return (
+    <div className='container'>
+      <h1 id={greeting} >Hello World!</h1>
+      <p>I'm writing JSX</p>
+      <ul>
+        <li>
+          <button>
+            <span role="img" aria-label="grinning face" id="grinning face" >&#x1F600;</span>
+          </button>
+        </li>
+        <li>
+          <button>
+            <span role="img" aria-label="party popper" id="party popper">&#x1F389;</span>
+          </button>
+        </li>
+        <li>
+          <button>
+            <span role="img" aria-label="woman dancing" id="woman dancing" >&#x1F483;</span>
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
