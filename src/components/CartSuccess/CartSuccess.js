1e6f35e943381ce692a10591969a7605f40ef58f
topcook <codecooker1996@gmail.com>
2022-01-17 02:38:14 -0800

    I added styling objects using hooks in CartSuccess component.

--- src/components/CartSuccess/CartSuccess.js
+++ src/components/CartSuccess/CartSuccess.js
@@ -1,36 +1,38 @@
 import React from 'react';
 import Alert from '../Alert/Alert';
 // import './CartSuccess.css';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+    item: {
+        marginRight:20,
+    },
+
+    wrapper: {
+        borderTop:'black solid 1px',
+        display:'flex',
+        flexWrap:'wrap',
+        '& h2': {
+            width:'100%'
+        }
+    }
+});
 
 export default function CartSuccess() {
 
-    const styles = {
-        wrapper: {
-            borderTop: 'black solid 1px',
-            display: 'flex',
-            flexWrap: 'wrap'
-        },
-        
-        header: {
-            width: '100%'
-        },
-        
-        item: {
-            marginRight: 20
-        }
-    }
+    const classes = useStyles();
 
     return (
         <Alert title="Added to Cart" type="success">
-            <div style = {styles.wrapper}>
-                <h2 style = {styles.header}>
+            <div className={classes.wrapper}>
+                <h2>
                     You have added 3 items:
                 </h2>
-                <div style = {styles.item}>
+                <div className={classes.item}>
                     <div>Bananas</div>
                     <div>Quantity: 2</div>
                 </div>
-                <div style = {styles.item}>
+                <div className={classes.item}>
                     <div>Lettuce</div>
                     <div>Quantity: 1</div>
                 </div>
