0ea1037e77a22aa523220bad899de3dba1a39e0e
topcook <codecooker1996@gmail.com>
2022-01-17 04:39:20 -0800

    I added the state variable: cart and total in Product component.

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import './Product.css';
 
 export default class Product extends Component {
+    state = {
+        cart: [],
+        total: 0
+    }
+
     render() {
         return (
             <div className="wrapper">
                 <div>
-                    Shopping Cart: 0 total items.
+                    Shopping Cart: {this.state.cart.length} total items.
                 </div>
-                <div>Total: 0</div>
+                <div>Total: {this.state.total}</div>
 
                 <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
                 <button>Add</button> <button>Remove</button>
