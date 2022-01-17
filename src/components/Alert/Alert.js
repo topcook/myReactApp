3fbeda2bf31a46832c0127311bae5a0fa235884a
topcook <codecooker1996@gmail.com>
2022-01-17 01:46:32 -0800

    I added the inline Javascript styling objects to Alert component.

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,11 +1,31 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import './Alert.css';
+// import './Alert.css';
 
 export default function Alert({children, title, type}) {
+
+    const colors = {
+        success: '#6da06f',
+        error: '#f56260'
+    }
+
+    const styles = {
+        heading: {
+            color: colors[type],
+            margin: '0 0 10px 0'
+        },
+
+        wrapper: {
+            border: `${colors[type]} solid 1px`,
+            marginBottom: 15,
+            padding: 15,
+            position: 'relative'
+        }
+    }
+
     return (
-        <div className={`alert-wrapper ${type}`}>
-            <h2>{title}</h2>
+        <div style = {styles.wrapper}>
+            <h2 style = {styles.heading}>{title}</h2>
             {children}
         </div>
     );
