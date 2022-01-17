5f917fc2957d228ff0efa7d15317031396214663
topcook <codecooker1996@gmail.com>
2022-01-17 00:13:35 -0800

    I added className `{alert-wrapper ${type}}` to div in Alert component. Here type is success or error.

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,9 +1,10 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import './Alert.css';
 
 export default function Alert({children, title, type}) {
     return (
-        <div>
+        <div className={`alert-wrapper ${type}`}>
             <h2>{title}</h2>
             {children}
         </div>
