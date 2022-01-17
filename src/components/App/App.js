f0a2a53c241181b22fb34be16ae28d4ec10ca791
topcook <codecooker1996@gmail.com>
2022-01-17 01:24:43 -0800

    I added dynamic inline styling to App.js.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+// import './App.css';
 import '../Alert/Alert';
 import Alert from '../Alert/Alert';
 import CartSuccess from '../CartSuccess/CartSuccess';
 
 function App() {
+
+  const wrapper =  {
+    padding:20
+  }
+
   return (
-    <div className='wrapper'>
+    <div style = {wrapper}>
       <Alert 
       title = "Itmes Not Added"
       type = "error">
