36a27d32e085c880a51942e96d8d16ff8b92e3a1
topcook <codecooker1996@gmail.com>
2022-01-16 23:36:08 -0800

    I added Alert component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import '../Alert/Alert';
+import Alert from '../Alert/Alert';
 
 function App() {
   return (
-    <></>
+    <div className='wrapper'>
+      <Alert />
+    </div>
   );
 }
 
