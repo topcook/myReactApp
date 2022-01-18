8062ac7864b0475f15a51f68cbb8311416f30d6b
topcook <codecooker1996@gmail.com>
2022-01-18 10:06:10 -0800

    I added textarea tag and textInformation component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,31 @@
+import React, { createContext, useState } from 'react';
 import './App.css';
+import TextInformation from '../TextInformation/TextInformation';
+
+export const TextContext = createContext();
+TextContext.displayName = 'TextContext';
 
 function App() {
+
+  const [text, setText] = useState('');
+
   return (
-    <></>
+    <TextContext.Provider value={text} >
+      <div className='wrapper'>
+        <label htmlFor='text'>
+          Add Your Text Here:
+          <br />
+          <textarea
+            id='text'
+            name='text'
+            rows='10'
+            cols='100'
+            onChange={(e) => setText(e.target.value)}
+          />
+        </label>
+        <TextInformation />
+      </div>
+    </TextContext.Provider>
   );
 }
 
