8b4f08250d8323d98f95f863c35cdfed842aa091
topcook <codecooker1996@gmail.com>
2022-01-18 02:31:10 -0800

    I added salad items in main page.

--- src/components/SaladItem/SaladItem.js
+++ src/components/SaladItem/SaladItem.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import { createUseStyles } from 'react-jss';
-import nodemon from "nodemon";
 
 const useStyles = createUseStyles({
     add: {
--- src/components/SaladMaker/SaladMaker.js
+++ src/components/SaladMaker/SaladMaker.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createUseStyles } from 'react-jss';
+import SaladBuilder from "../SaladBuilder/SaladBuilder";
 
 const useStyles = createUseStyles({
     wrapper: {
@@ -15,6 +16,7 @@ export default function SaladMaker() {
             <span role = "img" aria-label="salad">🥗</span>
             Build Your Custom Salad!
             <sapn role = "img" aria-label = "salad">🥗</sapn>
+            <SaladBuilder />
         </h1>
     );
 }
