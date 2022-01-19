3b956cc33095fa0e668ebcb62699580cbd9c30e2
topcook <codecooker1996@gmail.com>
2022-01-19 09:58:37 -0800

    I added the form to input item's name.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { getList } from '../../services/list';
+import { getList, setItem } from '../../services/list';
 
 function App() {
   const [list, setList] = useState([]);
+
+  const [itemInput, setIteminput] = useState('');
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    setItem(itemInput);
+  }
 
   useEffect(() => {
     let mounted = true;
@@ -22,6 +29,17 @@ function App() {
       <ul>
         {list.map(item => <li key={item.item}>{item.item}</li>)}
       </ul>
+      <p>You have entered: {itemInput}</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>New Item</p>
+          <input type="text" 
+          onChange={(e) =>setIteminput(e.target.value)}
+          value={itemInput}
+          />
+        </label>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   )
 }
