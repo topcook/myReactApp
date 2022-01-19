3110b7f31d71802ee4ae8bb3fa47b48a8d0d0544
topcook <codecooker1996@gmail.com>
2022-01-18 23:57:37 -0800

    I added submit event listener.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,25 @@
 import './App.css';
 
 function App() {
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert('You have submitted the form');
+  }
+
   return (
-    <></>
+    <div className="wrapper">
+      <h1>How About Them Apples</h1>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>Name</p>
+            <input name="name" />
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
 
