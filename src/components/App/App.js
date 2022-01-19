9e8131a769de62fe5f6517ff6808bbaeda755994
topcook <codecooker1996@gmail.com>
2022-01-19 13:08:22 -0800

    I added useSelector in App to pull data from store.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,28 @@
+import React from 'react';
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+
+  const bidrs = useSelector(state => state.bidrs);
+
   return (
-    <></>
-  );
+    <div class="wrapper">
+      <h1>Bird List</h1>
+      <ul>
+        {
+          bidrs.map((bird) => (
+            <li key={bird.name}>
+              <h2>{bird.name}</h2>
+              <div>Views: {bird.views}</div>
+              <button><span role="img" aria-label="add">➕</span></button>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  )
+
 }
 
 export default App;
