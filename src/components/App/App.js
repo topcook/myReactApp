a890f7e70a95a783f90326a496097a52ccab4dd9
topcook <codecooker1996@gmail.com>
2022-01-19 01:35:22 -0800

    I added new filedset to the form.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -45,6 +45,25 @@ function App() {
             <input name="name" onChange={handleChange} />
           </label>
         </fieldset>
+        <fieldset>
+          <label>
+            <p>Apples</p>
+            <select name="apple" onChange={handleChange}>
+              <option value="">--Please choose an option--</option>
+              <option value="fuji">Fuji</option>
+              <option value="jonathan">Jonathan</option>
+              <option value="honey-crisp">Honey Crisp</option>
+            </select>
+          </label>
+          <label>
+            <p>Count</p>
+            <input type="number" name="count" onChange={handleChange} step="1" />
+          </label>
+          <label>
+            <p>Gift Wrap</p>
+            <input type="checkbox" name="gift-wrap" onChange={handleChange} />
+          </label>
+        </fieldset>
         <button type="submit">Submit</button>
       </form>
     </div >
