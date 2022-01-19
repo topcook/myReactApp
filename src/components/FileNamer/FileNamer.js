d2700aab143e7ee68144667e6c17398efed955a9
topcook <codecooker1996@gmail.com>
2022-01-18 21:57:46 -0800

    I added validate function and show alert message when invalidated.

--- src/components/FileNamer/FileNamer.js
+++ src/components/FileNamer/FileNamer.js
@@ -4,19 +4,30 @@ import './FileNamer.css'
 export default function FileNamer() {
 
     const [name, setName] = useState('anonymous');
+    const [alert, setAlert] = useState(false);
+
+    const validate = (event) => {
+        if (/\*/.test(name)) {
+            event.preventDefault();
+            setAlert(true);
+            return;
+        }
+        setAlert(false);
+    }
 
     return (
         <div className="wrapper">
             <div className="preview">
-                <h2>Preview: { name}.js</h2>
+                <h2>Preview: {name}.js</h2>
             </div>
             <form>
                 <label>
                     <p>Name: </p>
                     <input autoComplete="off" name="name" onChange={event => setName(event.target.value)} />
                 </label>
+                { alert && <div>Forbidden Character: *</div>}
                 <div>
-                    <button>Save</button>
+                    <button onClick={validate}>Save</button>
                 </div>
             </form>
         </div>
