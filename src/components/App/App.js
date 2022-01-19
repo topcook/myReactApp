3552f9ba455b29e60431fc3f316ab821bec2f0b4
topcook <codecooker1996@gmail.com>
2022-01-19 04:49:07 -0800

    I added RiverInformation service and imported it in App component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,13 @@
+import React from 'react';
 import './App.css';
+import RiverInformation from '../RiverInformation/RiverInformation';
 
 function App() {
   return (
-    <></>
+    <div className="wrapper">
+      <h1>World's Longest Rivers</h1>
+      <RiverInformation />
+    </div>
   );
 }
 
