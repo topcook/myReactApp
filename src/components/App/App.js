a90628a46aec872f7c4104e8479783a9a619a242
topcook <codecooker1996@gmail.com>
2022-01-19 00:11:14 -0800

    I revised the submitting function.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,20 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
 
+  const [submitting, setSubmitting] = useState(false);
+
   const handleSubmit = event => {
     event.preventDefault();
-    alert('You have submitted the form');
+    setSubmitting(true);
+    setTimeout(() =>setSubmitting(false), 3000);
   }
 
   return (
     <div className="wrapper">
       <h1>How About Them Apples</h1>
+      {submitting && <div>Submiting Form...</div>}
       <form onSubmit={handleSubmit}>
         <fieldset>
           <label>
