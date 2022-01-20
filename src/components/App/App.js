7682c86245a422fad2b715b0bdaba345111be8f3
topcook <codecooker1996@gmail.com>
2022-01-20 04:29:06 -0800

    I revised the query parameter to another by using useParams hook.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,14 +15,15 @@ function App() {
             <li><Link to='/manatee'>Manatee</Link></li>
             <li><Link to='/narwhal'>Narwhal</Link></li>
             <li><Link to='/whale'>Whale</Link></li>
-            <li><Link to="/whale?type=beluga">Beluga Whale</Link></li>
-            <li><Link to="/whale?type=blue">Blue Whale</Link></li>
+            <li><Link to="/whale/beluga">Beluga Whale</Link></li>
+            <li><Link to="/whale/blue">Blue Whale</Link></li>
           </ul>
         </nav>
         <Routes>
           <Route path='/manatee' element={<Manatee />} />
           <Route path='/narwhal' element={<NarWhal />} />
-          <Route path='/whale' element={<Whale />} />
+          <Route exact path='/whale' element={<Whale />} />
+          <Route path='/whale/:type' element={<Whale />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Whale/Whale.js
+++ src/components/Whale/Whale.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Beluga from './Beluga';
 import Blue from './Blue';
 
 export default function Whale() {
-    const { search } = useLocation();
-    const match = search.match(/type=(.*)/);
-    const type = match?.[1];
+    const { type } = useParams();
 
     return (
         <>
