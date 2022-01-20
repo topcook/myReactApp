1a72e7cee4ef04113cef5423ab0db6fe4b4d1ce9
topcook <codecooker1996@gmail.com>
2022-01-20 10:19:25 -0800

    I added custom hook: useToken and imported it in App component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,19 +5,10 @@ import './App.css';
 import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
 import Login from '../Login/Login';
-
-function setToken(userToekn) {
-  sessionStorage.setItem('token', JSON.stringify(userToekn));
-}
-
-function getToken() {
-  const tokenString = sessionStorage.getItem('token');
-  const userToken = JSON.parse(tokenString);
-  return userToken?.token;
-}
+import useToken from './useToken';
 
 function App() {
-  const token = getToken();
+  const { token, setToken } = useToken();
 
   if (!token) {
     return <Login setToken={setToken} />
