978fd790371fba90ad7e0bcd1bfb39c4a55b100d
topcook <codecooker1996@gmail.com>
2022-01-19 23:35:45 -0800

    I added increment bird action and combined reducers.

--- src/store/birds/birds.js
+++ src/store/birds/birds.js
@@ -1,8 +1,17 @@
+import { combineReducers } from 'redux';
 const ADD_BIRD = 'ADD_BIRD';
+const INCREMENT_BIRD = 'INCREMENT_BIRD';
 
 export function addBird(bird) {
     return {
         type: ADD_BIRD,
+        bird
+    }
+}
+
+export function incrementBird(bird) {
+    return {
+        type: INCREMENT_BIRD,
         bird
     }
 }
@@ -24,7 +33,24 @@ function birds(state = defaultBirds, action) {
                     views: 1
                 }
             ]
+        case INCREMENT_BIRD:
+            const bird = state.find(b => action.bird === b.name);
+            const birds = state.filter(b => action.bird !== b.name);
+            return [
+                ...birds,
+                {
+                    ...bird,
+                    views: bird.views + 1
+                }
+            ];
         default:
             return state;
     }
 }
+
+const birdApp = combineReducers(
+    {
+        birds
+    });
+
+export default birdApp;
