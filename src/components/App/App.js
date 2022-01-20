a93f886a142f6bbbfd9208ecaf6857d4844e9e1b
topcook <codecooker1996@gmail.com>
2022-01-20 07:01:15 -0800

    I added dashboard and preferences component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
 
 function App() {
   return (
-    <></>
+    <div className='wrapper'>
+      <h2>Application</h2>
+      <BrowserRouter>
+        <nav>
+          <li><Link to='/dashboard'>Dashboard</Link></li>
+          <li><Link to='/preferences'>Preferences</Link></li>
+        </nav>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/preferences' element={<Preferences />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
