db75484acec4c636818e5185b6fa613bf0f2b9cf
topcook <codecooker1996@gmail.com>
2022-01-20 01:46:24 -0800

    I added nav and links to components.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import Manatee from '../Manatee/Manatee';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Whale from '../Whale/Whale';
+import NarWhal from '../Narwhal/Narwhal';
 
 function App() {
   return (
     <div className='wrapper'>
       <h1>Marine Mammals</h1>
+      <nav>
+        <ul>
+          <li><a href='/manatee'>Manatee</a></li>
+          <li><a href='/narwhal'>Narwhal</a></li>
+          <li><a href='/whale'>Whale</a></li>
+        </ul>
+      </nav>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Manatee />} />
+          <Route path='/manatee' element={<Manatee />} />
+          <Route path='/narwhal' element={<NarWhal />} />
           <Route path='/whale' element={<Whale />} />
         </Routes>
       </BrowserRouter>
