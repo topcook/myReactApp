e4c0c8ff47c5c2886989457b1911f3b81529f0ec
topcook <codecooker1996@gmail.com>
2022-01-20 13:38:04 -0800

    I added paragraph to explain the information about characters.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 import './App.css';
 
 import CharacterMap from '../CharacterMap/CharacterMap';
 
 function App() {
   const [text, setText] = useState('');
+  const [showExplanation, toggleExplanation] = useReducer(state => !state, false);
 
   return (
     <div className='wrapper'>
@@ -19,6 +20,14 @@ function App() {
         >
         </textarea>
       </label>
+      <div>
+        <button onClick={toggleExplanation}>Show Explanation</button>
+      </div>
+      {showExplanation &&
+        <p>
+          This displays a list of the most common characters.
+        </p>
+      }
       <CharacterMap text={text} />
     </div>
   );
