20a82c3b8635ced769bd0f893c30b41d76da9de3
topcook <codecooker1996@gmail.com>
2022-01-20 01:11:53 -0800

    I imported Manatee in App and added styling for class wrapper.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,13 @@
+import React from 'react';
 import './App.css';
+import Manatee from '../Manatee/Manatee';
 
 function App() {
   return (
-    <></>
+    <div className='wrapper'>
+      <h1>Marine Mammals</h1>
+      <Manatee />
+    </div>
   );
 }
 
