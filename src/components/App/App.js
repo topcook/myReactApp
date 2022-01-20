6daa41fbf8b4fafe98a7c84d400c61a3c59ca876
topcook <codecooker1996@gmail.com>
2022-01-20 10:39:17 -0800

    I added textarea to input from user.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <></>
+    <div className='wrapper'>
+      <label htmlFor='text'>
+        <p>Add your text here</p>
+        <textarea 
+        id='text'
+        name='text'
+        rows='10'
+        cols='100'>
+          
+        </textarea>
+      </label>
+    </div>
   );
 }
 
