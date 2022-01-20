51e55984da2e38cebfa89cbaafff7057a9e5d441
topcook <codecooker1996@gmail.com>
2022-01-20 08:21:53 -0800

    I added loginUser function to request and response from server.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,6 +2,17 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './Login.css';
 
+async function loginUser(credentials) {
+    return fetch('http://localhost:8080/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+    .then(data=>data.json())
+}
+
 export default function Login({ setToken }) {
 
     const [username, setUserName] = useState('');
