2b34a6b0de2e34a781c261caeeebe6ccbdd2a9d8
topcook <codecooker1996@gmail.com>
2022-01-20 08:00:49 -0800

    I added state for username and password.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './Login.css';
 
 export default function Login({ setToken }) {
+
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <div className='login=wrapper'>
             <h1>Please Login.</h1>
             <form>
                 <label>
                     <p>Username</p>
-                    <input type='text' />
+                    <input type='text' onChange={e => setUserName(e.target.value)} />
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type='password' />
+                    <input type='password' onChange={e => setPassword(e.target.value)} />
                 </label>
                 <div>
                     <button type='submit'>Submit</button>
