7ab8ea2e60468c02e814fdf9833033f5dae1c70f
topcook <codecooker1996@gmail.com>
2022-01-20 01:36:08 -0800

    I imported Manatee Component when '/'.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import Manatee from '../Manatee/Manatee';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className='wrapper'>
       <h1>Marine Mammals</h1>
-      <Manatee />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Manatee />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
