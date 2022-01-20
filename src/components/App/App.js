697032289e385687cf3f92b5abac27391ee24754
topcook <codecooker1996@gmail.com>
2022-01-20 09:07:45 -0800

    I added the routine to get and set token. But now, the browser doesnot retrieve the token.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,8 +6,18 @@ import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
 import Login from '../Login/Login';
 
+function setToken(userToekn) {
+  sessionStorage.setItem('token', JSON.stringify(userToekn));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token;
+}
+
 function App() {
-  const [token, setToken] = useState();
+  const token = getToken();
 
   if (!token) {
     return <Login setToken={setToken} />
