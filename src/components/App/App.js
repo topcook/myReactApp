cecefda0125aec104fcd03e7ff3dfd2205e79b0f
topcook <codecooker1996@gmail.com>
2022-01-20 07:08:51 -0800

    I added useState hook for login confirmation.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
+import Login from '../Login/Login';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className='wrapper'>
       <h2>Application</h2>
